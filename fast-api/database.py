d4f62bde625a47e956289fa8270db52b44eebace
NKoech123 <nicholask320@gmail.com>
2022-12-10 09:22:00 +0300

    add .env

--- fast-api/database.py
+++ fast-api/database.py
@@ -1,11 +1,15 @@
 import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
+from dotenv import load_dotenv
+from dotenv import dotenv_values
 
-DATABASE_URL = "postgresql://postgres:password@localhost:5432/fastapi_database"
-# DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+config = dotenv_values(".env")
+
+DATABASE_URL = config['DATABASE_URL']
+
 engine = _sql.create_engine(DATABASE_URL)
-print(engine.connect())
+
 SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = _declarative.declarative_base()
