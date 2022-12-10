e477775a67ad99187471c14f843498811b6d3546
NKoech123 <nicholask320@gmail.com>
2022-12-10 09:02:10 +0300

    fix/connect to dockerized postgres database

--- fast-api/database.py
+++ fast-api/database.py
@@ -2,10 +2,10 @@ import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
 
-DATABASE_URL = "postgresql://myuser:password@localhost/fastapi_database"
-
+DATABASE_URL = "postgresql://postgres:password@localhost:5432/fastapi_database"
+# DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 engine = _sql.create_engine(DATABASE_URL)
-
+print(engine.connect())
 SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = _declarative.declarative_base()
